e4422c606eaeb640b580ec4f3ae6b7c22ae0630d
Johannes Rupieper <Johannes.Rupieper@mac.com>
2021-12-30 08:28:26 +0100

    Update handler.js

--- handler.js
+++ handler.js
@@ -2,7 +2,7 @@ const fs = require('fs');
  
 module.exports = {
   main: async function (event, _) {
-   fs.readdirSync(testFolder).forEach(file => {
+   fs.readdirSync(__dirname).forEach(file => {
   console.log(file);
 });
     //const data = fs.readFileSync('./recordings.json', {encoding:'utf8', flag:'r'});
